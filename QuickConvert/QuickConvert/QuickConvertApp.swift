23ee2210e8bd784951352833948904ecd4d8d4de
N3v1 <n3v1010@gmail.com>
2023-08-18 18:17:41 +0200

    Create xcodeproj

---
+++ QuickConvert/QuickConvert/QuickConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickConvertApp.swift
+//  QuickConvert
+//
+//  Created by Nevio Hirani on 18.08.23.
+//
+
+import SwiftUI
+
+@main
+struct QuickConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
