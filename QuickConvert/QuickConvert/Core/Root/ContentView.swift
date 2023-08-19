80cf9e8dc4dfd072b749e2cf0407422a654039af
N3v1 <n3v1010@gmail.com>
2023-08-19 17:00:57 +0200

    Adding Launchscreen

--- QuickConvert/QuickConvert/Core/Root/ContentView.swift
+++ QuickConvert/QuickConvert/Core/Root/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selection: Tab = .Categories
-    
+    @State private var isLoading = false
     // Create enum for the Tab bar buttons
     enum Tab {
         case Categories
@@ -17,21 +17,53 @@ struct ContentView: View {
     }
     
     var body: some View {
-//        NavigationStack {
+        ZStack {
             TabView(selection: $selection) {
-                OptionListView()
-                    .tabItem {
-                        Label("List", systemImage: "list.bullet")
-                    }
-                    .tag(Tab.Categories)
+                    OptionListView()
+                        .tabItem {
+                            Label("List", systemImage: "list.bullet")
+                        }
+                        .tag(Tab.Categories)
 
-                FavouriteView()
-                    .tabItem {
-                        Label("Favourite", systemImage: "heart")
+                    FavouriteView()
+                        .tabItem {
+                            Label("Favourite", systemImage: "heart")
+                        }
+                        .tag(Tab.favourite)
+                }
+            // MARK: - Loading View
+            // TODO: Refactor LoadingView
+            if isLoading {
+                ZStack {
+                    Color(.systemBackground)
+                        .ignoresSafeArea()
+                    VStack {
+                        Spacer()
+                        
+                        Image("QuickConverter_for_doc")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                            .padding(50)
+                        
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .scaleEffect(2)
+                        
+                        Spacer()
                     }
-                    .tag(Tab.favourite)
+                }
             }
-//        }
+        }
+        .onAppear { startFakeNetworkCall() }
+    }
+    
+    // MARK: - UIActivityIndicator on start
+    func startFakeNetworkCall() {
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { 
+            isLoading = false
+        }
     }
 }
 
